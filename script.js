ecb010e927297f05b67cb07e9f1a832f75755f88
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-02-24 04:18:42 +0600

    2nd Commit

---
+++ script.js
@@ -0,0 +1,35 @@
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById("total");
+let interview = document.getElementById("interview");
+let rejected = document.getElementById("rejected");
+
+const allCardSection = document.getElementById('card')
+
+function calculateCount (){
+    total.innerText = allCardSection.children.length;
+    interview.innerText = interviewList.length;
+    rejected.innerText = rejectedList.length;
+}
+calculateCount();
+
+
+
+
+
+
+function toggle(id){
+    allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+    rejectedBtn.classList.remove('bg-[#3B82F6]', 'text-white');
+
+ 
+    allBtn.classList.add('bg-gray-100', 'text-black');
+    interviewBtn.classList.add('bg-gray-100', 'text-black');
+    rejectedBtn.classList.add('bg-gray-100', 'text-black');
+
+    const selected = document.getElementById(id);
+    selected.classList.remove('bg-gray-100', 'text-black');
+    selected.classList.add('bg-[#3B82F6]', 'text-white');
+}

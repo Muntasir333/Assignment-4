6a5bf24c67f6517643599097fa529da1fa939419
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-02-24 17:08:36 +0600

    5th commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let interviewList = [];
 let rejectedList = [];
+let currentStatus = 'allBtn';
 
 let total = document.getElementById("total");
 let interview = document.getElementById("interview");
@@ -22,6 +23,7 @@ calculateCount();
 
 
 function toggle(id){
+
     allBtn.classList.remove('bg-[#3B82F6]', 'text-white');
     interviewBtn.classList.remove('bg-[#3B82F6]', 'text-white');
     rejectedBtn.classList.remove('bg-[#3B82F6]', 'text-white');
@@ -32,6 +34,7 @@ function toggle(id){
     rejectedBtn.classList.add('bg-gray-100', 'text-black');
 
     const selected = document.getElementById(id);
+    currentStatus = id;
     selected.classList.remove('bg-gray-100', 'text-black');
     selected.classList.add('bg-[#3B82F6]', 'text-white');
 }
@@ -62,7 +65,7 @@ if (event.target.id === 'int-btn'){
     const description = parentN.querySelector('.description').innerText;
 
     const stat = parentN.querySelector('.status');
-    stat.innerText = 'INTERVIEW'
+    stat.innerText = 'Interview'
     stat.classList.add('border-[#10B981]', 'text-[#10B981]')
 
     const info = {
@@ -77,10 +80,44 @@ if (event.target.id === 'int-btn'){
     if(!exist){
         interviewList.push(info);
     }
-    calculateCount()
-  renderAppoint ()
+
+    rejectedList = rejectedList.filter(item=> item.companyName != info.companyName)
+    calculateCount();
+    if (currentStatus == "interviewBtn"){
+        renderAppoint ();
+    }
+
 }  
-})
+else if (event.target.id === 'rej-btn'){
+           const parentN = event.target.parentNode.parentNode;
+    const companyName = parentN.querySelector('.companyName').innerText;
+    const position = parentN.querySelector('.position').innerText;
+    const type = parentN.querySelector('.type').innerText;
+    const status = parentN.querySelector('.status').innerText;
+    const description = parentN.querySelector('.description').innerText;
+
+    const stat = parentN.querySelector('.status');
+    stat.innerText = 'Reject'
+    stat.classList.add('text-[#EF4444]', 'border-[#EF4444]')
+
+    const info = {
+        companyName, 
+        position, 
+        type, 
+        status, 
+        description
+    }
+    const exist = rejectedList.find(item=> item.companyName == info.companyName);
+    
+    if(!exist){
+        rejectedList.push(info);
+    }
+    interviewList = interviewList.filter(item=> item.companyName != info.companyName);
+    calculateCount()
+  renderReject ()
+}  
+}
+)
 
  
 
@@ -91,7 +128,6 @@ function renderAppoint (){
     recieved.innerHTML='';
 
         for (let inter of interviewList){
-            console.log(inter);
             let div = document.createElement('div');
             div.className = 'flex justify-between border p-8 mb-8'
             div.innerHTML = `    <div>
@@ -100,7 +136,7 @@ function renderAppoint (){
                 <br>
                 <p  class="type text-[#64748B] font-normal text-[14px]">${inter.type}</p>
                 <br>
-                <p class="status border-[#10B981] bg-[#EEF4FF] w-[150px] h-[30px] pl-8 py-2 text-[#10B981] font-normal text-[14px]">INTERVIEW</p>
+                <p class="status border-[#10B981] bg-[#EEF4FF] w-[150px] h-[30px] pl-8 py-2 text-[#10B981] font-normal text-[14px]">Interview</p>
                 <br>
                 <p class="description text-[#323B49] font-normal text-[14px]">${inter.description}</p>
                 <br>
@@ -115,6 +151,40 @@ function renderAppoint (){
             </div>
           `
           recieved.appendChild(div)
+        }
+
+}
+
+ const reject = document.getElementById('rRecieved')
+
+ function renderReject (){
+
+    reject.innerHTML='';
+
+        for (let rej of rejectedList){
+            let div = document.createElement('div');
+            div.className = 'flex justify-between border p-8 mb-8'
+            div.innerHTML = `    <div>
+                <h2 class="companyName text-[#002C5C] font-semibold text-[18px]">${rej.companyName}</h2>
+                <p  class="position text-[#64748B] font-normal text-[16px]">${rej.position}</p>
+                <br>
+                <p  class="type text-[#64748B] font-normal text-[14px]">${rej.type}</p>
+                <br>
+                <p class="status border-[#EF4444] bg-[#EEF4FF] w-[150px] h-[30px] pl-8 py-2 text-[#EF4444] font-normal text-[14px]">Rejected</p>
+                <br>
+                <p class="description text-[#323B49] font-normal text-[14px]">${rej.description}</p>
+                <br>
+                <div id="btn">
+                    <button id="int-btn" class="text-[#10B981] btn border-[#10B981]">INTERVIEW</button>
+                    <button id="rej-btn" class="btn text-[#EF4444] border-[#EF4444]">REJECTED</button>
+
+                </div>
+            </div>
+            <div>
+                <i class="fa-regular fa-trash-can"></i>
+            </div>
+          `
+          reject.appendChild(div);
         }
 
 }

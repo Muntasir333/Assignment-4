78e61aa8adcc99648da3da9ababa00f919d22934
Muntasir333 <mdfahimmuntasir333@gmail.com>
2026-02-24 12:27:48 +0600

    3rd commit

--- script.js
+++ script.js
@@ -4,11 +4,13 @@ let rejectedList = [];
 let total = document.getElementById("total");
 let interview = document.getElementById("interview");
 let rejected = document.getElementById("rejected");
+let count = document.getElementById('count');
 
 const allCardSection = document.getElementById('card')
 
 function calculateCount (){
     total.innerText = allCardSection.children.length;
+    count.innerText = allCardSection.children.length + ' ' + 'jobs';
     interview.innerText = interviewList.length;
     rejected.innerText = rejectedList.length;
 }
@@ -33,3 +35,21 @@ function toggle(id){
     selected.classList.remove('bg-gray-100', 'text-black');
     selected.classList.add('bg-[#3B82F6]', 'text-white');
 }
+
+
+function show(id){
+    const all = document.getElementById('card')
+    const recieved = document.getElementById('iRecieved')
+    const reject = document.getElementById('rRecieved')
+
+    all.classList.add('hidden');
+    recieved.classList.add('hidden');
+    reject.classList.add('hidden');
+
+    const select = document.getElementById(id);
+    select.classList.remove('hidden');
+}
+
+
+// const mainContainer = document.querySelector('main');
+
